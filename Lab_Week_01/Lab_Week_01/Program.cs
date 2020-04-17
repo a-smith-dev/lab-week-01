4f545aaaef549f606eb1aaf50aac08c97697310e
andy <asmithdev91@gmail.com>
2020-04-16 21:22:41 -0400

    First commit for Week 1, Lab 02.

---
+++ Lab_Week_01/Lab_Week_01/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Lab_Week_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal length, width, area, perimeter;
+            string repeater;
+            Console.WriteLine("Welcome to the Grand Circus' Room Detail Generator!");
+            do
+            {
+                Console.Write("\nEnter Length: ");
+                length = Convert.ToDecimal(Console.ReadLine());
+                Console.Write("Enter Width: ");
+                width = Convert.ToDecimal(Console.ReadLine());
+                area = length * width;
+                perimeter = (length + width) * 2;
+                Console.WriteLine("Area: " + area);
+                Console.WriteLine("Perimeter: " + perimeter);
+                Console.Write("Would you like to continue (y/n)? ");
+                repeater = Console.ReadLine();
+            } while (repeater.ToUpper() == "Y");
+            Console.WriteLine("Thank you for using the Grand Circus' Room Detail Generator! Have a great day!");
+        }
+    }
+}
